1ce3e3e7199bcdd38a4b4769b0904961c3d86c58
ravikishan.69 <ravikishan63392@gmail.com>
2024-10-19 00:45:20 +0530

    Adding ExpressCraft Project

--- components/data_projects.js
+++ components/data_projects.js
@@ -11,5 +11,16 @@ export const data = [
     tags: ["personal"],
     github: "https://github.com/ravikisha/pythoncentralhub",
     skills: ["Astro", "Python"],
-  }
+  },
+  {
+    name: "ExpressCraft",
+    description:
+    "A npx based express app generator for creating express apps with ease. It support all types of tools and technologies related to express.",
+    image: "expresscraft.png",
+    link: "https://www.npmjs.com/package/expresscraft",
+    linkedin: "https://www.linkedin.com/posts/ravikisha_expresscraft-nodejs-webdevelopment-activity-7250015815633174529-2FeF?utm_source=share&utm_medium=member_desktop",
+    tags: ["personal"],
+    github: "https://github.com/Ravikisha/ExpressCraft",
+    skills: ["JavaScript"]
+  },
 ];
